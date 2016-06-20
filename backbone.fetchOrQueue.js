24089caecf5fa932835d323f84ecf954f384115d
headlessme <andy@coggle.it>
2016-06-20 19:19:37 +0100

    simplify implementation

--- backbone.fetchOrQueue.js
+++ backbone.fetchOrQueue.js
@@ -2,40 +2,39 @@ define(function(require, exports, module) {
 
 Backbone.Model.prototype.fetchOrQueue = 
 Backbone.Collection.prototype.fetchOrQueue = function(callback, options) {
-  var resource = this;
+    var resource = this;
+    if (!options) options = {};
 
-  if (resource.loaded && ((Date.now() - resource.loaded.getTime()) < 30*1000)) {
-    if (callback) setTimeout(function() { callback(false, resource); }, 0);
-    return resource;
-  }
-
-  resource.once('sync', function(){
-      resource.loaded = new Date();
-      resource.fetching = false;
-  });
-  resource.once('error', function(){
-      resource.fetching = false;
-      resource.loaded = false;
-  });
-
-  if (callback) {
-    resource.once('sync', function() {
-      callback(false, resource);
-      resource.off(null, null, callback);
-    }, callback);
-
-    resource.once('error', function() {
-      callback(true);
-      resource.off(null, null, callback);
-    }, callback);
-
-    if (!resource.fetching) {
-      resource.fetch(options);
-      resource.fetching = true;
+    if (resource.loaded && ((Date.now() - resource.loaded.getTime()) < 2*60*1000)) {
+        if (callback) setTimeout(function() { callback(false, resource); }, 0);
+        return resource;
     }
-  }
+
+    if (callback) {
+        if (!resource.fetching) {
+
+        options.success = function() {
+            resource.loaded = new Date();
+            resource.fetching = false;
+            callback(false, resource);
+        };
+
+        options.error = function() {
+            resource.loaded = false;
+            resource.fetching = false;
+            callback(true);
+        };
+
+        resource.fetch(options);
+        resource.fetching = true;
+        } else {
+            resource.once('sync', function() {
+                resource.fetchOrQueue(callback, options);
+            });
+        }
+    }
   
-  return resource;
+    return resource;
 };
 
 });
